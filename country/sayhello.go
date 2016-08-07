a762dbd6ed26a1357aa29657eb9ae8c87967303b
Barrow Kwan <barrowkwan@gmail.com>
2016-08-06 23:45:11 -0700

    initial check in

---
+++ country/sayhello.go
@@ -0,0 +1,13 @@
+package country
+
+import "fmt"
+
+func Say_USA()  {
+	fmt.Printf("Hello, USA\n")
+
+}
+
+func Say_Canada()  {
+	fmt.Printf("Hello, Canada\n")
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/barrowkwan/gocd_golang_hello/country"
+
+func main()  {
+	country.Say_USA()
+}

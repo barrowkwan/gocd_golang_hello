2088360665d979c064c8f4d6839997c108164729
Barrow Kwan <barrowkwan@gmail.com>
2016-08-08 13:21:24 -0700

    Add test to build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export GOPATH
 
 rm -rf $GOPATH/bin/*
 
-go test -v $GOPATH/bin/helloworld github.com/barrowkwan/gocd_golang_hello/country
+go test -v github.com/barrowkwan/gocd_golang_hello/country
 
 GOOS=darwin go build -o $GOPATH/bin/helloworld github.com/barrowkwan/gocd_golang_hello
 

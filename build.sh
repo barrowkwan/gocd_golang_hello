dc1d319f58898d7aaca9c6ad082d5df75e806b69
Barrow Kwan <barrowkwan@gmail.com>
2016-08-09 20:38:59 -0700

    Add test reporting

--- build.sh
+++ build.sh
@@ -5,7 +5,16 @@ export GOPATH
 
 rm -rf $GOPATH/bin/*
 
-go test -v github.com/barrowkwan/gocd_golang_hello/country
+if [[ -f $GOPATH/bin/go-junit-report ]]; then
+  echo "Remove binary : $GOPATH/bin/go-junit-report"
+  rm -rf $GOPATH/bin/go-junit-report
+fi
+go build -o $GOPATH/bin/go-junit-report github.com/jstemmer/go-junit-report
+if [[ -f testreport.xml ]]; then
+  echo "Remove old test report : testreport.xml"
+  /bin/rm -rf testreport.xml
+fi
+go test -v github.com/barrowkwan/gocd_golang_hello... | $GOPATH/bin/go-junit-report > testreport.xml
 
 GOOS=darwin go build -o $GOPATH/bin/helloworld github.com/barrowkwan/gocd_golang_hello
 
